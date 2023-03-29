3aba38cd0b189980f01696e7006e6ab426a68c37
mirandamanuel <49087932+mirandamanuel@users.noreply.github.com>
2023-03-29 15:16:45 -0700

    Update MainActivity

--- app/src/main/java/com/glitchypuma/hypermarketsifter/MainActivity.kt
+++ app/src/main/java/com/glitchypuma/hypermarketsifter/MainActivity.kt
@@ -2,10 +2,23 @@ package com.glitchypuma.hypermarketsifter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.topbar_menu, menu)
+        return true
     }
 }
