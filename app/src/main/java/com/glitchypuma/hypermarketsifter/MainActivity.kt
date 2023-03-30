18665709173da9c692b77446ebe1aa1a142a3153
mirandamanuel <49087932+mirandamanuel@users.noreply.github.com>
2023-03-30 15:11:57 -0700

    Update MainActivity action menu

--- app/src/main/java/com/glitchypuma/hypermarketsifter/MainActivity.kt
+++ app/src/main/java/com/glitchypuma/hypermarketsifter/MainActivity.kt
@@ -5,15 +5,45 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var toolbar: Toolbar
+    lateinit var fab: FloatingActionButton
+    lateinit var newPhotoButton: ExtendedFloatingActionButton
+    lateinit var openPhotosButton: ExtendedFloatingActionButton
+    lateinit var inputManuallyButton: ExtendedFloatingActionButton
+    var newInputMenuHidden: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        fab = findViewById(R.id.floating_action_button)
+        newPhotoButton = findViewById(R.id.new_photo_button)
+        openPhotosButton = findViewById(R.id.open_photos_button)
+        inputManuallyButton = findViewById(R.id.input_manually_button)
+
+        fab.setOnClickListener(){
+            if(newInputMenuHidden){
+                newPhotoButton.show()
+                openPhotosButton.show()
+                inputManuallyButton.show()
+                newInputMenuHidden = false
+            } else {
+                newPhotoButton.hide()
+                openPhotosButton.hide()
+                inputManuallyButton.hide()
+                newInputMenuHidden = true
+            }
+        }
+
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
