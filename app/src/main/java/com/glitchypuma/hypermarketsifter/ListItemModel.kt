d316ed78b90626c4a810062a848352288508e78d
mirandamanuel <49087932+mirandamanuel@users.noreply.github.com>
2023-04-24 10:50:50 -0700

    Delete ui.main folder -- app runs

--- app/src/main/java/com/glitchypuma/hypermarketsifter/ListItemModel.kt
+++ app/src/main/java/com/glitchypuma/hypermarketsifter/ListItemModel.kt
@@ -1,14 +1,5 @@
 package com.glitchypuma.hypermarketsifter
 
 class ListItemModel(val itemName: String, val itemPrice: String, val itemType: String) {
-    fun getItemName() : String {
-        return itemName
-    }
-    fun getItemPrice() : String {
-        return itemPrice
-    }
-    fun getItemType() : String {
-        return itemType
-    }
 
 }
--- app/src/main/java/com/glitchypuma/hypermarketsifter/SifterAdapter.kt
+++ app/src/main/java/com/glitchypuma/hypermarketsifter/SifterAdapter.kt
@@ -6,13 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import org.w3c.dom.Text
 
 class SifterAdapter(val context : Context, val listItemModels : ArrayList<ListItemModel>) : RecyclerView.Adapter<SifterAdapter.SifterViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SifterViewHolder {
         val inflater : LayoutInflater = LayoutInflater.from(context)
-        val view : View = inflater.inflate(R.layout.recycler_view_row, parent, false)
+        val view : View = inflater.inflate(R.layout.sifter_rv_row, parent, false)
         return SifterViewHolder(view)
     }
 
@@ -21,9 +20,9 @@ class SifterAdapter(val context : Context, val listItemModels : ArrayList<ListIt
     }
 
     override fun onBindViewHolder(holder: SifterViewHolder, position: Int) {
-        holder.tvItem.setText(listItemModels.get(position).getItemName())
-        holder.tvPrice.setText(listItemModels.get(position).getItemPrice())
-        holder.tvType.setText(listItemModels.get(position).getItemType())
+        holder.tvItem.setText(listItemModels.get(position).itemName)
+        holder.tvPrice.setText(listItemModels.get(position).itemPrice)
+        holder.tvType.setText(listItemModels.get(position).itemType)
     }
 
     inner class SifterViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
